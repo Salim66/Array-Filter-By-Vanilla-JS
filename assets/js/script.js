bdbeef9893741237e6bea53b9fec1579599c6708
Salim66 <salimhasanriad8@gmail.com>
2021-08-01 09:47:22 +0600

    Gender wise filter done

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ let users = [
     skill : 'JavaScript Dev',
     cell  : '01773980593',
     age   : '25',
+    gender    : 'Male',
     location  : 'Mirpur',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -15,6 +16,7 @@ let users = [
     skill : 'Python Dev',
     cell  : '01659874589',
     age   : '30',
+    gender    : 'Female',
     location  : 'Uttara',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -24,6 +26,7 @@ let users = [
     skill : 'Networking Sec',
     cell  : '01256987548',
     age   : '26',
+    gender    : 'Male',
     location  : 'Mirpur',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -33,6 +36,7 @@ let users = [
     skill : 'Laravel Dev',
     cell  : '01369587425',
     age   : '32',
+    gender    : 'Female',
     location  : 'Mirpur',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -42,6 +46,7 @@ let users = [
     skill : 'Seo Analist',
     cell  : '01698523698',
     age   : '29',
+    gender    : 'Male',
     location  : 'Bonani',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -51,6 +56,7 @@ let users = [
     skill : 'MERN Dev',
     cell  : '01785963256',
     age   : '35',
+    gender    : 'Female',
     location  : 'Uttara',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -60,6 +66,7 @@ let users = [
     skill : 'JavaScript Dev',
     cell  : '01569854789',
     age   : '23',
+    gender    : 'Female',
     location  : 'Bonani',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -69,6 +76,7 @@ let users = [
     skill : 'Laravel Dev',
     cell  : '01456321547',
     age   : '26',
+    gender    : 'Male',
     location  : 'Uttara',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -78,6 +86,7 @@ let users = [
     skill : 'Networking Sec',
     cell  : '01856325698',
     age   : '26',
+    gender    : 'Male',
     location  : 'bonani',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -87,6 +96,7 @@ let users = [
     skill : 'Java Dev',
     cell  : '01740445607',
     age   : '15',
+    gender    : 'Female',
     location  : 'Mirpur',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -96,6 +106,7 @@ let users = [
     skill : 'Wordpress Dev',
     cell  : '01569874525',
     age   : '26',
+    gender    : 'Male',
     location  : 'Uttara',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -105,6 +116,7 @@ let users = [
     skill : 'Laravel Dev',
     cell  : '01236589874',
     age   : '21',
+    gender    : 'Female',
     location  : 'Bonani',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -114,6 +126,7 @@ let users = [
     skill : 'Laravel Dev',
     cell  : '01456325896',
     age   : '35',
+    gender    : 'Female',
     location  : 'Uttara',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -123,6 +136,7 @@ let users = [
     skill : 'JavaScript Dev',
     cell  : '01985632587',
     age   : '40',
+    gender    : 'Female',
     location  : 'Mirpur',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -132,6 +146,7 @@ let users = [
     skill : 'PHP Dev',
     cell  : '01563258963',
     age   : '25',
+    gender    : 'Male',
     location  : 'Bonani',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -141,6 +156,7 @@ let users = [
     skill : 'JavaScript Dev',
     cell  : '01773980593',
     age   : '23',
+    gender    : 'Male',
     location  : 'Mirpur',
     desc  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, odio.',
   },
@@ -149,6 +165,7 @@ let users = [
 
 // get selectors
 let team = document.querySelector('#team');
+let gender = document.querySelectorAll('input[name="gender"]');
 
 
 showUsers();
@@ -169,4 +186,30 @@ function showUsers(){
                         </div>
                       </div>`;
   });
+}
+
+
+// search by gender
+for(let i = 0; i < gender.length; i++){
+  gender[i].onchange = () => {
+    team.innerHTML = '';
+    let gen = gender[i].value;
+    users.map(user => {
+      if(user.gender.toLocaleLowerCase() == gen || gen == ''){
+        team.innerHTML += `<div class="col-md-4 mb-4">
+                            <div class="card shadow cd">
+                              <div class="card-body slider">
+                                <img class="shadow-lg" src="users/${user.photo}" alt="">
+                                <h2>${user.name}</h2>
+                                <h5>${user.skill}</h5>
+                                <h5>${user.cell}</h5>
+                                <h6>${user.age} years old</h6>
+                                <h6>${user.location}</h6>
+                                <p>${user.desc}</p>
+                              </div>
+                            </div>
+                          </div>`;
+      }
+    });
+  }
 }
